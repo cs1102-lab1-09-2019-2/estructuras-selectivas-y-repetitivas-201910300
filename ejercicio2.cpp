c021811935cffd8564f55419e432b33864735cda
201910300 <54295635+201910300@users.noreply.github.com>
2019-09-01 22:24:05 -0500

    Update ejercicio2.cpp

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,6 +1,42 @@
 
+#include<iostream>
 
-int main() {
+using std::cout;
+using std::cin;
 
-    return 0;
+void imprimir(int n){
+  int filas,columnas;
+  
+  
+  for(filas=0;filas<n;filas++)
+  {   
+    for(columnas=0;columnas<n;columnas++)
+    {
+      if (filas%2==0)
+      { 
+        
+        if(columnas==filas || columnas==0 || columnas==n-1 ||filas==0 || filas==n-1)
+          cout<<"*";
+        else
+           cout<<" ";
+      }
+      else
+      { 
+        if(columnas==filas || columnas==0 || columnas==n-1 ||filas==0 || filas==n-1)
+          cout<<"*";
+        else
+          cout<<" ";
+       }
+    
+    }
+    printf("\n");
+  }
+  
+}
+
+int main()
+{ int n;
+  cout<<"Ingrese el tamaño de la n:";
+  cin>>n;
+  imprimir(n);
 }
